7cd5634fb9695c71b60fe985cd6e93284536f120
cire1776 <cire1776@gmail.com>
2021-04-01 12:23:37 -0400

    implement footer links

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -6,7 +6,9 @@ function MainFooter() {
     <footer>
       <p>(216) 334-6644 • hudsonmargo@yahoo.com</p>
       <aside>
-        © 2021 Margo Hudson. Created by Alpha-Pegasus Media using Gatsby.
+        © 2021 Margo Hudson. Created by{" "}
+        <a href="https://www.alphapegasus.us">Alpha-Pegasus Media</a> using{" "}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>.
       </aside>
     </footer>
   );
